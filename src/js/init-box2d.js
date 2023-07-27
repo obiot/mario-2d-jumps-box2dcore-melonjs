b55aa66467f9e0f2d37d0a9647db15e6569a7e73
Olivier Biot <olivier.biot@me.com>
2023-07-27 23:22:57 +0800

    Step the Box2D world AFTER the internal update loop

--- src/js/init-box2d.js
+++ src/js/init-box2d.js
@@ -6,19 +6,18 @@ let world = null;
 
 
 function initBox2D() {
-
-    const gravity = new b2Vec2(0, 9.8);
-
     // create the b2World
-    world  = new b2World(gravity);
+    world  = new b2World(new b2Vec2(0, 9.8));
     
     // update the box2D world every frame
-    event.on(event.GAME_UPDATE, (dt) => {
-        world.Step(dt, { velocityIterations: 3, positionIterations: 2 });
+    event.on(event.GAME_AFTER_UPDATE, (dt) => {
+        if (world.GetBodyCount() > 0) {
+            //console.log("update box2d world :" + dt / 1000 + "ms");
+            world.Step(dt / 1000, { velocityIterations: 3, positionIterations: 2 });
+        }
     });
 
     return world;
-
 }
 
 export default initBox2D;

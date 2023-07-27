5fdc1c786df83e99b113020e8280942479170f0b
Olivier Biot <olivier.biot@me.com>
2023-07-27 23:25:44 +0800

    DebugDrawer fixes and improvements
    - fix TextureCanvas not being refreshed when using the WebGL renderer
    - call DebugDraw in the draw method (so after the Box2D world was updated)
    - only drawShapes if the debugPanel is visible

--- src/js/debug-drawer.js
+++ src/js/debug-drawer.js
@@ -1,4 +1,4 @@
-import { Renderable, CanvasTexture } from "melonjs";
+import { Renderable, CanvasTexture, plugins } from "melonjs";
 import { DebugDraw } from "@box2d/debug-draw";
 import { DrawShapes, DrawAABBs } from "@box2d/core";
 import global from "./global.js";
@@ -13,14 +13,28 @@ export default class DebugDrawer extends Renderable {
     }
 
     update(dt) {
-        this.debugDraw.Prepare(this.width / 2, this.height / 2, 1, false);
-        //DrawShapes(this.debugDraw, global.b2World, false);
-        DrawAABBs(this.debugDraw, global.b2World, false);
-        this.debugDraw.Finish();
-        return true;
+        return global.b2World.GetBodyCount() > 0;
     }
 
     draw(renderer) {
-        renderer.drawImage(this.offScreenCanvas.canvas);
+        // only draw if the debug panel is visible
+        if (plugins.debugPanel && plugins.debugPanel.panel.visible) {
+            // skip if the world is empty
+            if (global.b2World.GetBodyCount() > 0) {
+                // TODO: replace with `invalidate(renderer)` once version 15.8 is published
+                if (typeof renderer.gl !== "undefined") {
+                    // make sure the right compositor is active
+                    renderer.setCompositor("quad");
+                    // invalidate the previous corresponding texture so that it can reuploaded once changed
+                    this.glTextureUnit = renderer.cache.getUnit(renderer.cache.get(this.offScreenCanvas.canvas));
+                    renderer.currentCompositor.unbindTexture2D(null, this.glTextureUnit);
+                }
+                this.debugDraw.Prepare(this.width / 2, this.height / 2, 1, false);
+                //DrawAABBs(this.debugDraw, global.b2World, false);
+                DrawShapes(this.debugDraw, global.b2World, false);
+                this.debugDraw.Finish();
+                renderer.drawImage(this.offScreenCanvas.canvas);
+            }
+        }
     }
 }

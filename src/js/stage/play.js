b93e5c8408b2dcf51b31cbe085ffb63aecac666e
Olivier Biot <olivier.biot@me.com>
2023-07-27 10:59:28 +0800

    set the background color directly in the Tiled map

--- src/js/stage/play.js
+++ src/js/stage/play.js
@@ -6,9 +6,6 @@ class PlayScreen extends Stage {
      *  action to perform on state change
      */
     onResetEvent() {
-        // add a gray background to the default Stage
-        game.world.addChild(new ColorLayer("background", "#202020"));
-        
         // load a level
         level.load("simple-level");
 

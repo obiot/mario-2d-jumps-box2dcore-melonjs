460765d26753f596e7d8a11bbb07b76d4b596f22
Olivier Biot <olivier.biot@me.com>
2023-07-27 23:37:58 +0800

    removing unused BitmapFont assets from the boilerplate

--- src/manifest.js
+++ src/manifest.js
@@ -1,11 +1,6 @@
 // a melonJS data manifest
 // note : this is note a webpack manifest
 const DataManifest = [
-
-    /* Bitmap Text */
-    { name: "PressStart2P", type: "image", src:  "./data/fnt/PressStart2P.png" },
-    { name: "PressStart2P", type: "binary", src: "./data/fnt/PressStart2P.fnt" },
-
     /* Maps */
     { name: "simple-level",             type: "tmx",    src: "data/map/simple_level.tmx" },
     { name: "simple_level_tileset",     type: "tsx",    src: "data/map/simple_level_tileset.tsx" },
